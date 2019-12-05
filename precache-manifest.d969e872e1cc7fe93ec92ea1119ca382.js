6e3a3a4b79a97cf909d07107397dd263ae5669ff
jessie Diaz <wahyah117@gmail.com>
2019-12-05 13:05:06 -0800

    Updates

---
+++ precache-manifest.d969e872e1cc7fe93ec92ea1119ca382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c384c2ad8a74cdec611aeba7a20ea8e0",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "672d83ec40d3789814c4",
+    "url": "/RoboFriends/static/css/main.ced433c9.chunk.css"
+  },
+  {
+    "revision": "10ca8a264ab078a444e0",
+    "url": "/RoboFriends/static/js/2.49b130ef.chunk.js"
+  },
+  {
+    "revision": "672d83ec40d3789814c4",
+    "url": "/RoboFriends/static/js/main.5b792c3e.chunk.js"
+  },
+  {
+    "revision": "6239f40d0ef81a2fd1e0",
+    "url": "/RoboFriends/static/js/runtime-main.6d5bc549.js"
+  }
+]);
